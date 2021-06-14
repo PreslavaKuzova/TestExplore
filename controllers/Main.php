91b51bfb3539be93bdedf44704d0e99250b67954
Teodor Kostadinov <teodor.kostadinov@volasoftware.com>
2021-06-14 19:20:31 +0300

    Arch

---
+++ controllers/Main.php
@@ -0,0 +1,17 @@
+<?php
+
+class Main extends Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+
+        $this->view->render('main/index');
+
+    }
+}
