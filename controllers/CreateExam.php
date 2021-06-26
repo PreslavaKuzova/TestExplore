beacf552e77308b8254d7913a602e9d2d1b3e8a7
Teodor Kostadinov <teodor.kostadinov@ad.mentormate.bg>
2021-06-27 00:42:18 +0300

    Create exam functionality

--- controllers/CreateExam.php
+++ controllers/CreateExam.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once 'controllers/BaseController.php';
+require_once 'models/dto/Answer.php';
+require_once 'models/dto/Exam.php';
+require_once 'models/dto/Question.php';
 
 class CreateExam extends BaseController
 {
@@ -25,6 +28,44 @@ class CreateExam extends BaseController
     }
 
     public function save() {
+        //TODO Remove this when done debugging
+        echo '<pre>';
+        print_r($_POST);
+        echo '</pre>';
 
+        $examName = $_POST["exam-name"];
+        $accessCode = $_POST["access-code"];
+        $examLevel = $_POST["exam-level"];
+        $teacherId = $_SESSION["teacher_id"];
+        $date = date("y-m-d");
+        $questions = array();
+        $questionIndex = 1;
+
+        while (true) {
+            if(isset($_POST["question-title-" . $questionIndex])) {
+                $content = $_POST["question-title-" . $questionIndex];
+                $answerIndex = 1;
+                $answers = array();
+                while (true) {
+                    if (isset($_POST["answer-" . $questionIndex . "-" . $answerIndex])) {
+                        $ans = $_POST["answer-" . $questionIndex . "-" . $answerIndex];
+                        $cor = isset($_POST["correct-" . $questionIndex . "-" . $answerIndex]);
+                        $answers[] = new Answer($ans, $cor);
+                        $answerIndex++;
+                    } else {
+                        break;
+                    }
+                }
+                $questions[] = new Question($content, "multiple", $answers);
+                $questionIndex++;
+            } else {
+                break;
+            }
+        }
+
+        $exam = new Exam($examName, $accessCode, $date, $examLevel, $teacherId, $questions);
+
+        //TODO Replace this with database insert
+        echo json_encode($exam);
     }
 }
--- models/dto/Question.php
+++ models/dto/Question.php
@@ -3,11 +3,13 @@ class Question
 {
     //FIXME The fields are public in order for the json_encode to be able to read them.
     //It can be implemented in another way but much more complicated
+    public $questionContent;
     public $questionType;
     public $answers;
 
-    function __construct($questionType, $answers)
+    function __construct($questionContent, $questionType, $answers)
     {
+        $this->questionContent = $questionContent;
         $this->questionType = $questionType;
         $this->answers = $answers;
     }
@@ -20,5 +22,10 @@ class Question
     function getAnswers()
     {
         return $this->answers;
+    }
+
+    public function getQuestionContent()
+    {
+        return $this->questionContent;
     }
 }
