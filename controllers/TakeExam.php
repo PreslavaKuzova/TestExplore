7556c2848de815cc267fa56af2f14c667ef8ede4
Borislava <bbardarska@gmail.com>
2021-06-29 13:48:21 +0300

    Added css for TakeExam page

--- controllers/TakeExam.php
+++ controllers/TakeExam.php
@@ -38,13 +38,13 @@ class TakeExam extends BaseController
     {
         //TODO Replace with database call
         return new Exam("-1", "Test Exam of Mathematics", "", "2021-05-10", "5", "1", array(
-            new Question("1", "What is a*b?", "multiple", array(new Answer("Rectangle", true), new Answer("Square", false))),
-            new Question("2","What is pi?", "multiple", array(new Answer("3.14", true), new Answer("22/7", false), new Answer("a circle", false), new Answer("yummmy", true))),
-            new Question("3","Which is bigger: 2 on power of 3 or 3 on the power of 2?", "multiple", array(new Answer("2^3", false), new Answer("3 ^ 2", false), new Answer("yo mamma", true), new Answer("they are equal", false))),
-            new Question("4","What is a*b?", "multiple", array(new Answer("Rectangle", true), new Answer("Square", false))),
-            new Question("5","What is a*b?", "multiple", array(new Answer("Rectangle", true), new Answer("Square", false))),
-            new Question("6","What is a*b?", "multiple", array(new Answer("Rectangle", true), new Answer("Square", false))),
-            new Question("7","What is a*b?", "multiple", array(new Answer("Rectangle", true), new Answer("Square", false))),
+            new Question("1", "Question 1: 3x−25y+4 is an example of an algebraic:", "multiple", array(new Answer("expression", true), new Answer("operation", false), new Answer("quotient", false),new Answer("equation", false))),
+            new Question("2","Question 2: What is pi?", "multiple", array(new Answer("3.14", true), new Answer("22/6", false), new Answer("6.28", false), new Answer("0", false))),
+            new Question("3","Question 3: Simplify: 3x^2+2x−7x^2", "multiple", array(new Answer("−2x^5", false), new Answer("10x^2−2x", false), new Answer("−4x^2+2x", true), new Answer("−2x^2", false))),
+            new Question("4","Question 4: Solve this equation: 5−2(x+3)=13", "multiple", array(new Answer("x=−6", false), new Answer("x=−7", true), new Answer("x=−1", false), new Answer("x=4/3", false))),
+            new Question("5","Question 5: x=−3 is a solution to which of these equations?", "multiple", array(new Answer("−6=−2x", false), new Answer("2x−1=x+7", false), new Answer("x+5=−2", false), new Answer("x^2+x−6=0", true))),
+            new Question("6","Question 6: Solve the inequality: −2x+4>10", "multiple", array(new Answer("x<−3", true), new Answer("x≥−3", false), new Answer("x=−3", false), new Answer("x>−3", false))),
+            new Question("7","Question 7: What is the coefficient in this expression: 3x2y", "multiple", array(new Answer("x", false), new Answer("3", true), new Answer("y", false), new Answer("2", false))),
         ));
     }
 
